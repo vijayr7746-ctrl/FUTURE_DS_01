9938907dc29271bd99cca7615a6469de58bb3d25
vijayr7746-ctrl <karthikvijay7746@gmail.com>
2026-05-20 00:32:09 +0530

    Update FUTURE_DS_01.py

--- FUTURE_DS_01.py
+++ FUTURE_DS_01.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load Dataset
-df = pd.read_csv(r"C:\Users\Vijay\Desktop\presidency\PRO\Superstore1.csv", encoding='latin1')
+df = pd.read_csv(r"C:\Users\Vijay\Desktop\presidency\PROJECT\1\Superstore1.csv", encoding='latin1')
 # Show First 5 Rows
 print(df.head())
 
@@ -13,15 +13,13 @@ print(df.isnull().sum())
 # Convert Order Date to Date Format
 df['Order Date'] = pd.to_datetime(df['Order Date'])
 
-# ==============================
 # TOTAL SALES
-# ==============================
+
 total_sales = df['Sales'].sum()
 print("Total Sales:", total_sales)
 
-# ==============================
 # TOP 10 PRODUCTS
-# ==============================
+
 top_products = df.groupby('Product Name')['Sales'].sum().sort_values(ascending=False).head(10)
 
 print(top_products)
@@ -34,9 +32,8 @@ plt.ylabel("Sales")
 plt.xticks(rotation=75)
 plt.show()
 
-# ==============================
 # SALES BY CATEGORY
-# ==============================
+
 category_sales = df.groupby('Category')['Sales'].sum()
 
 print(category_sales)
@@ -47,9 +44,8 @@ plt.title("Sales by Category")
 plt.ylabel("")
 plt.show()
 
-# ==============================
 # SALES TREND OVER TIME
-# ==============================
+
 monthly_sales = df.groupby(df['Order Date'].dt.to_period('M'))['Sales'].sum()
 
 monthly_sales.plot(figsize=(12,5))
@@ -58,9 +54,8 @@ plt.xlabel("Month")
 plt.ylabel("Sales")
 plt.show()
 
-# ==============================
 # REGION WISE SALES
-# ==============================
+
 region_sales = df.groupby('Region')['Sales'].sum()
 
 print(region_sales)
@@ -71,9 +66,8 @@ plt.xlabel("Region")
 plt.ylabel("Sales")
 plt.show()
 
-# ==============================
 # PROFIT ANALYSIS
-# ==============================
+
 profit = df.groupby('Category')['Profit'].sum()
 
 profit.plot(kind='bar', color='green')
@@ -82,9 +76,8 @@ plt.xlabel("Category")
 plt.ylabel("Profit")
 plt.show()
 
-# ==============================
 # BUSINESS INSIGHTS
-# ==============================
+
 print("\nBusiness Insights:")
 print("1. Identify high revenue products.")
 print("2. Focus on profitable categories.")
